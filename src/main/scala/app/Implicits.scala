d81b4601f86af83be440e68fbc52b243b003fb72
Dominik 'squ@ll' Jasiński <dominikjasinski@o2.pl>
2019-03-29 21:00:59 +0100

    Implicits typeclasses

---
+++ src/main/scala/app/Implicits.scala
@@ -0,0 +1,28 @@
+package app
+
+object Implicits {
+  def reduceLeftTest[B: Numeric](data: Seq[B])(implicit num: Numeric[B]) = {
+    data.reduceLeft((a, b) => num.plus(a, b))
+  }
+
+  def reduceLeftTestOwnFunc[B: Numeric](data: Seq[B])(op: (B, B) => B)(implicit num: Numeric[B]) = {
+    data.reduceLeft((a, b) => op(a, b))
+  }
+
+  def foldLeftTest[B: Numeric](data: Seq[B])(implicit num: Numeric[B]) = {
+    data.foldLeft(num.zero)((a, b) => num.plus(a, b))
+  }
+
+  def sumTest[B: Numeric](data: Seq[B])(implicit num: Numeric[B]) = {
+    data.sum(num)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val list = List(1, 2.1, 3.32)
+
+    println(reduceLeftTest(list))
+    println(reduceLeftTestOwnFunc(list)((x, y) => x * y))
+    println(foldLeftTest(list))
+    println(sumTest(list))
+  }
+}
